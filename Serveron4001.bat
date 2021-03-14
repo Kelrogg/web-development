8edb037188593322d3408209935a2a74b1db4151
Kelrogg <61114591+Kelrogg@users.noreply.github.com>
2021-03-14 15:19:36 +0300

    Delete Serveron4001.bat

--- Serveron4001.bat
+++
@@ -1,5 +0,0 @@
-echo off
-set /p fileName="Enter file name: "
-for /r %%i in (*.cgi) do del %%i
-for /r %%k in (*exe) do ren %%k %fileName%.cgi
-start "" http://localhost:4001/cgi-bin/%fileName%.cgi?name
